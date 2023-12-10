f0bc8d3de089b8dc98edbe42c76795edd051c6b6
fabio-cerundolo <cerundolo.fabio@gmail.com>
2023-12-10 20:25:02 +0100

    Hellow

--- workspace/hello-selenium/src/main/java/demo/TestLoginPage.java
+++ workspace/hello-selenium/src/main/java/demo/TestLoginPage.java
@@ -1,6 +1,6 @@
 package demo;
 
-import java.time.Duration;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,19 +11,39 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestLoginPage {
     public static void main(String[] args) {
-        System.out.println(proceduraLogin("tomsmith", "SuperSecretPassword!"));
+        String url = "http://the-internet.herokuapp.com/login";
+        String broswer="chrome";
+        String messaggio_di_ritorno =proceduraLogin("tomsmith", "SuperSecretPassword!", url,broswer);
+        System.out.println(messaggio_di_ritorno+ broswer);
+        broswer="edge";
+        messaggio_di_ritorno =proceduraLogin("tomsmith", "SuperSecretPassword!", url,broswer);
+        System.out.println(messaggio_di_ritorno+ broswer);
+
 
     
 }
 
-private static String proceduraLogin(String user, String pass) {
+private static String proceduraLogin(String user, String pass, String url, String broswer) {
         String message=null;
-        WebDriver driver = new ChromeDriver();
-		System.setProperty("webdriver.chrome.driver", "chromedriver");
-        
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriver driver =null;
+       // WebDriver driver = new ChromeDriver();
+       switch(broswer){
+           case "chrome":driver = new ChromeDriver();
+                        System.setProperty("webdriver.chrome.driver", "chromedriver");
+                        
+                        break;
+           default : driver = new org.openqa.selenium.edge.EdgeDriver();
+                       System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
+                        
+                        break;
+                        
 
-		driver.get("http://the-internet.herokuapp.com/login");
+       }
+		
+        
+		
+
+		driver.get(url);
         WebElement username = driver.findElement(By.name("username"));
         WebElement password = driver.findElement(By.name("password"));
         WebElement invio = driver.findElement(By.tagName("button"));
@@ -36,20 +56,20 @@ private static String proceduraLogin(String user, String pass) {
         String UrlAttuale = driver.getCurrentUrl();
         
         if(UrlAttuale.equals(UrlArrivo)) {
-            System.out.println("Login Succesfully!");
-            WebElement SuccesMessage = driver.findElement(By.id("flash"));
-            System.out.println(SuccesMessage.getText());
+            message="Login Succesfully: "+ user + " " + pass+ " ";
+          //  WebElement SuccesMessage = driver.findElement(By.id("flash"));
+           // System.out.println(SuccesMessage.getText());
            // WebElement LogOutButton = driver.findElement(By.xpath("//a[@class='button secondary radius']"));
            // LogOutButton.click();
         }
         else {
             WebElement successMessage = driver.findElement(By.id("flash"));
             if(successMessage.getText().substring(0,13).equals("Your username")){
-                message="Username errato: "+user;
+                message="Username errato: "+user+ " ";
 
             }
             if (successMessage.getText().substring(0,13).equals("Your password")) {
-                message="Password errata: "+pass;
+                message="Password errata: "+pass+" ";
             }
 
             System.out.println("Login Failed!");
